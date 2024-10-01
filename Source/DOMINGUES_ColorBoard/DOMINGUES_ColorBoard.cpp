3df96b008b4c8bc1e10fb33dcd314baf38f62499
Domingues romain <romain.Jvfan@gmail.com>
2024-10-01 15:49:49 +0200

    c++

---
+++ Source/DOMINGUES_ColorBoard/DOMINGUES_ColorBoard.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DOMINGUES_ColorBoard.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DOMINGUES_ColorBoard, "DOMINGUES_ColorBoard" );
---
+++ Source/DOMINGUES_ColorBoard/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class DOMINGUES_COLORBOARD_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
